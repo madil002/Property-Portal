9b1748f778bbfefc9ee793a82f453906f810453a
madil002 <muabdul.adil@gmail.com>
2023-12-09 14:24:33 +0000

    Added duplication check for registering

--- routes/main.js
+++ routes/main.js
@@ -19,24 +19,38 @@ module.exports = function(app, appData) {
     app.post('/registered', function (req,res) {
         const plainPassword = req.body.password;
 
-        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        let checkForDupe = "SELECT * FROM users WHERE username = ? OR email = ?";
+        db.query(checkForDupe, [req.body.username, req.body.email], function(err, result){
             if (err) {
                 console.error(err.message);
-                res.redirect('./');
-            }
-            else {
-                let sqlquery = "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES (?,?,?,?,?)";
-                let newrecord = [req.body.username, hashedPassword, req.body.email, req.body.first, req.body.last];
-                db.query(sqlquery, newrecord, (err, result) => {
+                res.redirect('./register');
+            } else if (result.length > 0){
+                let newData = Object.assign({}, appData, { error: "Username or email already exists. Please try again." });
+                res.render('register.ejs', newData);
+            } else{
+                bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
                     if (err) {
-                        return console.error(err.message);
+                        console.error(err.message);
+                        res.redirect('./register');
                     }
                     else {
-                        res.redirect('./');
+                        let sqlquery = "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES (?,?,?,?,?)";
+                        let newrecord = [req.body.username, hashedPassword, req.body.email, req.body.first, req.body.last];
+                        db.query(sqlquery, newrecord, (err, result) => {
+                            if (err) {
+                                console.error(err.message);
+                                res.redirect('./');
+                            }
+                            else {
+                                res.redirect('./');
+                            }
+                        })
                     }
-                })
+                  })
             }
-          })
+        })
+
+
           
     }); 
 
