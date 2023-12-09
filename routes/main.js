cf55c23a9ec1f812d8d941e9c11ec833b37d6e0f
madil002 <muabdul.adil@gmail.com>
2023-12-09 14:05:35 +0000

    Added registering functionality with hashing

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
 module.exports = function(app, appData) {
 
-    // Handle our routes
+    // Routes
     app.get('/',function(req,res){
         res.render('index.ejs', appData)
     });
@@ -12,6 +15,30 @@ module.exports = function(app, appData) {
     app.get('/register',function(req,res){
         res.render('register.ejs', appData)
     });
+
+    app.post('/registered', function (req,res) {
+        const plainPassword = req.body.password;
+
+        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+            if (err) {
+                console.error(err.message);
+                res.redirect('./');
+            }
+            else {
+                let sqlquery = "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES (?,?,?,?,?)";
+                let newrecord = [req.body.username, hashedPassword, req.body.email, req.body.first, req.body.last];
+                db.query(sqlquery, newrecord, (err, result) => {
+                    if (err) {
+                        return console.error(err.message);
+                    }
+                    else {
+                        res.redirect('./');
+                    }
+                })
+            }
+          })
+          
+    }); 
 
     app.get('/login',function(req,res){
         res.render('login.ejs', appData)
