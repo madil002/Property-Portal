aef9e626e2f80cb237237ed77b1294ec79c69382
madil002 <muabdul.adil@gmail.com>
2023-12-11 14:42:07 +0000

    Added server side input validation

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
+const { check, validationResult } = require('express-validator');
 
 module.exports = function (app, appData) {
 
@@ -25,38 +26,50 @@ module.exports = function (app, appData) {
     });
 
     //Need to add server side validation for the form fields
-    app.post('/registered', function (req, res) {
-        const plainPassword = req.body.password;
+    app.post('/registered',
+    [check('email').isEmail().withMessage('Invalid email format'),
+    check('username').isLength({ min: 4 }).withMessage('Username must be at least 4 characters long').matches(/^[a-zA-Z0-9]+$/).withMessage('Username must contain only alphanumeric characters'),
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long').matches(/\d/).withMessage('Password must contain a number'),
+    check('first').not().isEmpty().withMessage('First name is required'),
+    check('last').not().isEmpty().withMessage('Last name is required')], function (req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const reason = errors.array().map(err => err.msg);
+            res.render('register.ejs', Object.assign({}, appData, { error : reason }));
+        }
+        else {
+            const plainPassword = req.body.password;
 
-        let checkForDupe = "SELECT * FROM users WHERE username = ? OR email = ?";
-        db.query(checkForDupe, [req.body.username, req.body.email], function (err, result) {
-            if (err) {
-                console.error(err.message);
-                res.redirect('./register');
-            } else if (result.length > 0) {
-                res.render('register.ejs', Object.assign({}, appData, { error: "Username or email already exists. Please try again." }));
-            } else {
-                bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
-                    if (err) {
-                        console.error(err.message);
-                        res.redirect('./register');
-                    }
-                    else {
-                        let sqlquery = "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES (?,?,?,?,?)";
-                        let newrecord = [req.body.username, hashedPassword, req.body.email, req.body.first, req.body.last];
-                        db.query(sqlquery, newrecord, (err, result) => {
-                            if (err) {
-                                console.error(err.message);
-                                res.redirect('./');
-                            }
-                            else {
-                                res.render('register.ejs', Object.assign({}, appData, { success: "Registration successful!" }));
-                            }
-                        })
-                    }
-                })
-            }
-        })
+            let checkForDupe = "SELECT * FROM users WHERE username = ? OR email = ?";
+            db.query(checkForDupe, [req.body.username, req.body.email], function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    res.redirect('./register');
+                } else if (result.length > 0) {
+                    res.render('register.ejs', Object.assign({}, appData, { error: "Username or email already exists. Please try again." }));
+                } else {
+                    bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+                        if (err) {
+                            console.error(err.message);
+                            res.redirect('./register');
+                        }
+                        else {
+                            let sqlquery = "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES (?,?,?,?,?)";
+                            let newrecord = [req.body.username, hashedPassword, req.body.email, req.body.first, req.body.last];
+                            db.query(sqlquery, newrecord, (err, result) => {
+                                if (err) {
+                                    console.error(err.message);
+                                    res.redirect('./');
+                                }
+                                else {
+                                    res.render('register.ejs', Object.assign({}, appData, { success: "Registration successful!" }));
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
     });
 
     app.get('/login', function (req, res) {
